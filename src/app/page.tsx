944eb4dde17439bcae3e654013c62b358ef88198
Emanuele Strano <>
2023-11-08 14:30:26 +0100

    fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="relative container flex min-h-screen flex-col">
       <div className=" p-4 flex h-14 items-center justify-between supports-backdrop-blur:bg-background/60 sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur">
-        <span className="font-bold">EUI Vectorized DBs into conversational chat<br/><span className=""><small>An AI Experiment by Emanuele Strano</small></span></span>
+        <span className="font-bold">EUI Vectorized DBs into conversational chat<br/><span className=""><small>An AI Experiment by EUI Webunit - info at emanuele.strano@eui.eu</small></span></span>
         
         <DarkModeToggle />
       </div>
--- src/lib/langchain.ts
+++ src/lib/langchain.ts
@@ -12,13 +12,17 @@ Chat History:
 Follow Up Input: {question}
 Standalone question:`;
 
-const QA_TEMPLATE = `You are an enthusiastic AI assistant. Use the following pieces of context to answer the question at the end in the language of the user.
-If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
-If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
+const QA_TEMPLATE = `You are an AI assistant for the EUI (European University Institute) located in Florence, Italy. Use the following pieces of context to answer the user question, in the language of the user.
+If you don't know the answer, just say you don't know or ask for more info. DO NOT try to make up an answer.
+If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the EUI.
+
+<<START CONTEXT>>
 
 {context}
 
-Question: {question}
+<<END CONTEXT>>
+
+User question: {question}
 Helpful answer in markdown:`;
 
 function makeChain(
@@ -32,7 +36,7 @@ function makeChain(
   // const writer = transformStream.writable.getWriter();
 
   const streamingModel = new ChatOpenAI({
-    modelName: "gpt-3.5-turbo",
+    modelName: "gpt-4-1106-preview",
     streaming: true,
     temperature: 0,
     verbose: true,
