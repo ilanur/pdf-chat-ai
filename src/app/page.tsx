4a11200d05d3f5dedc0888b53dccc4a18b73f54b
Emanuele Strano <>
2023-11-10 15:20:34 +0100

    fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,11 @@ import { Chat } from "@/components/chat";
 export default function Home() {
   return (
     <main className="relative container flex min-h-screen flex-col">
-      <div className=" p-4 flex h-14 items-center justify-between supports-backdrop-blur:bg-background/60 sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur">
-        <span className="font-bold">EUI Vectorized DBs into conversational chat<br/><span className=""><small>An AI Experiment by EUI Webunit - info at emanuele.strano@eui.eu</small></span></span>
-        
+      <div className="p-4 flex h-15 items-center justify-between supports-backdrop-blur:bg-background/60 sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur">
+        <span className="font-bold">ICT Web Unit - Intranet Tools
+        <br/><span className=""><small>Testing vector databases into conversational chat. Info @ Emanuele Strano</small></span>
+        <br/><span className=""><small>This AI is trained on only 3 documents and the answers are limited to 3 vectors per query.</small></span>
+        </span>
         <DarkModeToggle />
       </div>
       <div className="flex flex-1 py-4">
